ed89d44d28d578e3c1438277619a707a624f8b63
Primrizzle <116143986+Primrizzle@users.noreply.github.com>
2023-04-30 02:48:34 -0500

    Create ttt_functions.hpp
    Header file for my tictactoe game. All function declarations are present on this file.

---
+++ cpp/functions/TicTacToe/ttt_functions.hpp
@@ -0,0 +1,16 @@
+#ifndef ttt_functions_hpp
+#define ttt_functions_hpp
+
+#include <stdio.h>
+
+#endif /* ttt_functions_hpp */
+
+//Declare functions below
+void draw();
+void greet();
+void take_turn();
+void change_turns();
+bool is_filled();
+void end_game();
+bool scratch();
+
